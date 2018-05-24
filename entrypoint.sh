cd1dcab6d5869cf716fa4a6ea745539a3ed6ea77
刘勉 <mian.liu@lanmaoly.com>
2018-05-24 18:24:08 +0800

    支持多仓库

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,28 @@ eval "cat <<EOF
 EOF" >> $1
 }
 
+function is_empty_dir(){ 
+  return `ls -A $1|wc -w`
+}
 
+function new_repo() {
+  if is_empty_dir $1; then
+    svnadmin create --fs-type fsfs $1
+  else
+    echo repo $1 created, skip
+  fi
+}
 
-if [ ! -d '/repo' ];then
-  echo create /repo
-  svnadmin create --fs-type fsfs /repo
-fi
+mkdir -p /repo
+for item in `ls /repo`; do
+  REPO=/repo/$item
+  if [ -d $REPO ];then
+    # 是目录
+    echo find $REPO
+    new_repo $REPO
+  fi
+done
+
 
 # Apache gets grumpy about PID files pre-existing
 rm -f /run/apache2/httpd.pid
